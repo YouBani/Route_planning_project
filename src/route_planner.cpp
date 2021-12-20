4251af0ff580f195fb773badedb52bc2d0003f2b
Youness Bani <91743838+YouBani@users.noreply.github.com>
2021-12-20 15:15:34 +0100

    add_neighbor changes

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -8,30 +8,38 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
-    // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    // Find the closest nodes to the starting and ending coordinates
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 
 }
 
-
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    return node->distance(*end_node);
 }
 
 
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
-// Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
 // - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
+// - For each node in current_node.neighbors, 
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+
+    // Populate current_node.neighbors vector with all the neighbors
+    current_node->FindNeighbors();
+
+    // Go through each current_node neighbors, set the parent, the h_value, the g_value
+    // Add each neighbor to open_list and set the node's visited attribute to true.
+
+    for(auto neighbor : current_node->neighbors){
+        neighbor->parent = current_node;
+        neighbor->h_value = CalculateHValue(neighbor);
+        neighbor->g_value = g_value + current_node->distance(*neighbor);
+        open_list.emplace_back(neighbor);
+        neighbor->visited = true;
+    }
+
+
 
 }
 
